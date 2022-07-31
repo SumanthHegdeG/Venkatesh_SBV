7410f750e0bd1fe8f4d58bd5e8c16257b5903055
ajithhegde2 <ajith.hegde2@gmail.com>
2022-07-31 15:52:24 +0530

    footer added

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-
+import { MdContactPhone, MdEmail } from 'react-icons/md'
 const Footer = () => {
   return (
     <footer style={{ backgroundColor: 'black', color: 'white' }}>
       <Container>
-        <Row>
-          <Col className='m-3' md={6} sm={12}>
+        <Row className='flex-md-row flex-column'>
+          <Col className='m-3 '>
             <h4 className='text-white'>SBV INFOTECH</h4>
-            <p>Phone:9448276944/8073916173</p>
-            <p> SBVINFOTEK@GMAIL.com</p>
+            <div className='ml-3'>
+              <p>
+                <strong>
+                  Phone <MdContactPhone style={{ fontSize: '24px' }} /> :{' '}
+                </strong>
+                9448276944/8073916173
+              </p>
+              <p>
+                <strong>
+                  E-mail <MdEmail style={{ fontSize: '24px' }} /> :{' '}
+                </strong>{' '}
+                SBVINFOTEK@GMAIL.com
+              </p>
+            </div>
             <h5 style={{ color: 'white' }}>GSTIN-29AFCPV7553A1ZA</h5>
           </Col>
           <Col className='m-3'>
             <h5 style={{ color: 'white' }}>Address :</h5>
-            <p>Chitpavana suvarna bhavana #507,</p>
-            <p> 65th cross, 5th block,</p>
-            <p>rajajinagar,</p>
-            <p> bengaluru-560010</p>
+            <div className='ml-3'>
+              <p>Chitpavana suvarna bhavana #507,</p>
+              <p> 65th cross, 5th block,</p>
+              <p>rajajinagar,</p>
+              <p> bengaluru-560010</p>
+            </div>
           </Col>
         </Row>
+        <hr style={{ backgroundColor: 'grey' }} />
         <Row>
           <Col className='text-center py-3'>Copyright &copy; SBV Infotech</Col>
         </Row>
