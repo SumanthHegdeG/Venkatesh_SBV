3828794feab02aa1fae70dcb95022315036292a5
ajithhegde2 <ajith.hegde2@gmail.com>
2023-09-12 14:00:21 +0530

    final

---
+++ frontend/build/precache-manifest.64912ee7842efc5cc13edce3d12a3565.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c10f934970e0f0ad8158e29d9d292dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387d2b54b31871cf8ce7",
+    "url": "/static/css/main.fc12d33d.chunk.css"
+  },
+  {
+    "revision": "5ec455836f8b3c10a002",
+    "url": "/static/js/2.cfada486.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/static/js/2.cfada486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387d2b54b31871cf8ce7",
+    "url": "/static/js/main.ebc471d9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ebc471d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "7ce08319d0898e63e893ad90140b01ac",
+    "url": "/static/media/SBV-Logo.7ce08319.png"
+  },
+  {
+    "revision": "46494d973c7f514e0708401935f28ad7",
+    "url": "/static/media/aboutUsImg.46494d97.jpg"
+  },
+  {
+    "revision": "13315e6e5e080a9ea21b9735a2f68053",
+    "url": "/static/media/homeImg.13315e6e.png"
+  }
+]);
